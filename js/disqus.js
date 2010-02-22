43874a7cabef6a94ad65f397d9bf972dfbd38c07
Bartek Szopka <bartek.szopka+github@gmail.com>
2010-02-22 20:54:20 +0100

    comments lazy loading

--- js/disqus.js
+++ js/disqus.js
@@ -1,17 +1,31 @@
-// Loads DISQUS comments on a post page
+(function () {
 
-// include DISQUS when page loaded
-$(document).ready(function(){
-  $("#disqus_thread").html("<p class='loading'>Loading comments, please wait...</p>");
+// Loads DISQUS comments on a post page
+loadDisqus = function () {
+  $("#disqus_thread")
+    .addClass('loading')
+    .html("<p class='loading'>Loading comments, please wait...</p>");
   $.getScript("http://disqus.com/forums/itsabodybuildingblog/embed.js");
 
   // because disqus doesn't have any reasonable 'loaded' event
   // we need to fake it with some polling
   var waitForDisqus = setInterval(function () {
     if($("#dsq-comments").length > 0) {
-      $('#disqus_thread .loading').remove();
+      $('#dsq-content').hide();
+      $('#disqus_thread').removeClass('loading')
+        .find('.loading').remove();
+      $('#dsq-content').slideDown();
       clearInterval(waitForDisqus);
     }
   }, 100);
+
+  return false;
+};
+
+// include DISQUS when page loaded
+$(document).ready(function () {
+  $("#disqus_thread a[rel='disqus']").click(loadDisqus);
 });
+
+})();
 
